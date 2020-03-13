a8e79389fca9ec686360834282b6e30f502cd0be
Sergii Pylypenko <x.pelya.x@gmail.com>
2020-03-13 22:46:18 +0200

    Test to allocate maximum amount of RAM and crash

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ function printKey(e) {
     newCodeText = e.code || '';
   }
 
-  let text = "Keycode " + e.keyCode.toString() + " value " + newKeyText + " code " + newCodeText;
+  let text = "event.keyCode=" + e.keyCode.toString() + " event.key=" + newKeyText + " event.code=" + newCodeText;
 
   console.log(text);
   var element = document.getElementById('output');
@@ -32,3 +32,19 @@ function printKey(e) {
 };
 
 document.addEventListener('keydown', printKey);
+
+var growArray = [];
+
+function growMemory() {
+  growArray.push(new Uint8Array(1024 * 1024)); // 1Meg native arrays
+  for (var i = 0; i < 1024 * 1024; i++) {
+    growArray[growArray.length - 1][i] = Math.floor(Math.random() * 256);
+  }
+  var text = "Allocated " + growArray.length.toString() + " Mb of RAM";
+  var element = document.getElementById('output');
+  if (element) {
+    element.value = text + "\n" + element.value;
+  }
+}
+
+setInterval(growMemory, 200);
